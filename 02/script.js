e8f4a339b2257d784dc8dddfa65bb0ca31371f14
github-@irahuldutta02 <rdtech2002@gmail.com>
2023-02-19 16:27:40 +0530

    first commit

---
+++ 02/script.js
@@ -0,0 +1,7 @@
+var fullName = "Rahul Dutta";
+var courseName = "Reactjs Online";
+var isLoggedIn = false;
+var loggedCount = 34;
+var paymenMode;
+paymenMode = "card";
+console.log(fullName);
---
+++ 05/05_datatypes.js
@@ -0,0 +1,4 @@
+"use strict"
+
+// alert(3+3); // nodejs can not run this
+
