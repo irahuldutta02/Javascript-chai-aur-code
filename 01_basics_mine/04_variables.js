48023796ed9131f615ccad1009ae1a345f9e1a68
github-@irahuldutta02 <rdtech2002@gmail.com>
2023-02-25 20:32:54 +0530

    25-02-2023

---
+++ 01_basics_mine/04_variables.js
@@ -0,0 +1,8 @@
+const accountId = 10;
+let accountEmail = "rdtech2002@gmail.com";
+var accountPassword = "1234";
+accountCity = "Midnapore";
+
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
